0addd3e5f2f944ab08e3795ca803a88d091ab675
walarro <103528375+walarro@users.noreply.github.com>
2022-04-14 13:41:59 -0700

    I made it mildly worse

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var goosePos = 10;
 let goosePosY = 340;
 var twX = 300;
-var knife = 0
+var knife = 0;
+var knifeX = 0;
+var knifeY = 0;
 
 function setup() {
   createCanvas(600, 400);
@@ -16,41 +18,48 @@ function draw() {
   if (keyIsDown(RIGHT_ARROW)) {
     goosePos = goosePos + 2.5;
   }
-  
+
+  // Knife functionality
+  if (knife > 600) {
+    knife = "n/a";
+  }
   if (keyIsDown(75)) {
-    if (knife < 200){
-      knife = knife + 4
+    for (let i = 0; i < 2.5; i = i + 0.25) {
+      knifeX = 40;
+      knifeY = 10;
+      knife = knife + 0.75;
     }
   }
 
-  // Moving the jump function down to line 44 for better functionality
-  /* if(keyIsDown(UP_ARROW)){
-    goosePosY = goosePosY + 10
-  } */
+  if (knife > twX - 20 && knife < twX - 10) {
+    twX = "n/a";
+    knife = "n/a";
+  }
+
   // makes sure the goose don't get too high
-  
+
   if (goosePosY < 340) {
     goosePosY = goosePosY + 0.8;
   }
   frameRate(60);
   background("#69EEFF");
-  
+
   //floor
   noStroke();
   fill("#8BC34A");
   rect(0, 350, 600, 50);
   noFill();
-  
+
   // trash warriors
-  fill("rgb(46,45,45)");  
+  fill("rgb(46,45,45)");
   rect(twX, 320, 50, 50);
-  
-  // goose
-  fill("white");
-  rect(goosePos, goosePosY, 30, 30);
-  
+
   // violent flying objects
-  rect(knife,320,40,10)
+  fill("white");
+  rect(knife + goosePos, 340, knifeX, knifeY);
+
+  // goose
+  rect(goosePos, goosePosY, 30, 30);
 }
 
 // The goose jumps higher than a kindergartener who had 1 grain of sugar
