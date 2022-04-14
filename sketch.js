ee58b7bf58f0afb1375b632dd61a7aa944964a9e
walarro <103528375+walarro@users.noreply.github.com>
2022-04-14 08:09:33 -0700

    I worked on a knife that don't work

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var goosePos = 10;
 let goosePosY = 340;
 var twX = 300;
+var knife = 0
 
 function setup() {
   createCanvas(600, 400);
@@ -15,8 +16,19 @@ function draw() {
   if (keyIsDown(RIGHT_ARROW)) {
     goosePos = goosePos + 2.5;
   }
+  
+  if (keyIsDown(75)) {
+    if (knife < 200){
+      knife = knife + 4
+    }
+  }
 
+  // Moving the jump function down to line 44 for better functionality
+  /* if(keyIsDown(UP_ARROW)){
+    goosePosY = goosePosY + 10
+  } */
   // makes sure the goose don't get too high
+  
   if (goosePosY < 340) {
     goosePosY = goosePosY + 0.8;
   }
@@ -30,12 +42,15 @@ function draw() {
   noFill();
   
   // trash warriors
-  fill("gray");
+  fill("rgb(46,45,45)");  
   rect(twX, 320, 50, 50);
   
   // goose
   fill("white");
   rect(goosePos, goosePosY, 30, 30);
+  
+  // violent flying objects
+  rect(knife,320,40,10)
 }
 
 // The goose jumps higher than a kindergartener who had 1 grain of sugar
